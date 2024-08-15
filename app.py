0f738afdcd27e388235b8dbbf91743860f291a19
José Otávio Junqueira Ramos <167153159+JoseOtavioJunqueira@users.noreply.github.com>
2024-08-15 15:48:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ scheduler.start()
 atexit.register(lambda: scheduler.shutdown(wait=False))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context='adhoc')
